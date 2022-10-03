852652b235f7388a3118e58452376000676c5c66
Artem <artp80@gmail.com>
2022-10-03 16:42:07 +0400

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const fsp = require("fs/promises");
+const prompt = require('prompt-sync')();
+const path = require("path")
+
+console.log("Enter filename:")
+const fileName = prompt()
+
+fsp.readFile(path.resolve(__dirname, fileName), {encoding:"UTF-8"}).then(
+    data => console.log(data)
+).catch(err => console.log(err))
