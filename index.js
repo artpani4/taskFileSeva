ffe567dc260d8350843d062482f8581ecbb095e6
Artem <artp80@gmail.com>
2022-10-03 16:49:10 +0400

    Читает из консоли имя файла Х, затем читает файл Х выводит содержимое в консоль

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const path = require("path")
 console.log("Enter filename:")
 const fileName = prompt()
 
-fsp.readFile(path.resolve(__dirname, fileName), {encoding:"UTF-8"}).then(
-    data => console.log(data)
-).catch(err => console.log(err))
+fsp.readFile(path.resolve(__dirname, fileName), {encoding:"UTF-8"})
+    .then(
+    fname => {
+        fsp.readFile(path.resolve(__dirname, fname),{encoding:"UTF-8"})
+            .then(data=>console.log(data))
+    }
+)
+    .catch(err => console.log(err))
