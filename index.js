a21b94d16cb64ec22e3924d433c51d227ed44110
Artem <artp80@gmail.com>
2022-10-03 17:12:33 +0400

    Fixed Переписал на async/await

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const fsp = require("fs/promises");
 const prompt = require('prompt-sync')();
 const path = require("path")
 
-console.log("Enter filename:")
-const fileName = prompt()
 
-fsp.readFile(path.resolve(__dirname, fileName), {encoding:"UTF-8"})
-    .then(
-    fname => {
-        fsp.readFile(path.resolve(__dirname, fname),{encoding:"UTF-8"})
-            .then(data=>console.log(data))
+const main = async ()=>{
+    console.log("Enter filename:")
+    const fileName = prompt()
+    try{
+        const firstFile = await fsp.readFile(path.resolve(__dirname, fileName), {encoding:"UTF-8"})
+        const data = await fsp.readFile(path.resolve(__dirname, firstFile),{encoding:"UTF-8"})
+        console.log(data)
     }
-)
-    .catch(err => console.log(err))
+    catch (err){
+        console.log(err)
+    }
+}
+
+
+main().then()
